cca37d14e34faec74f6d58dba31749c847199660
jeshi2 <jamesvean19@gmail.com>
2024-01-22 20:31:49 +0300

    forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -21,6 +21,13 @@ def unauthorized_endpoint():
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint():
+    """ Forbidden endpoint
+    """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
