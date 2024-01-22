1610901e59f7aef0190abc34bbe6835c4670bbb6
jeshi2 <jamesvean19@gmail.com>
2024-01-22 20:25:16 +0300

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,11 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """ Unauthorized endpoint
+    """
+    abort(401)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
