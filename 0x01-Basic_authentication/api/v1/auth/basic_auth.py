4a88613ab76be7768c0d5678003c4298f0f01667
jeshi2 <jamesvean19@gmail.com>
2024-01-22 21:42:11 +0300

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth module
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -24,3 +25,21 @@ class BasicAuth(Auth):
         """
         base64_part = authorization_header.replace("Basic ", "", 1)
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode Base64 Authorization Header method
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            """
+            Decode the Base64 string
+            """
+            decoded_value = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decoded_value
+        except Exception:
+            return None
