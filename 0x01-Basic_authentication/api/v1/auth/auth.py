d8651a7c71d5642e05ae2bb6b3013c6303683ea2
jeshi2 <jamesvean19@gmail.com>
2024-01-22 21:14:30 +0300

    no auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,29 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require Auth method
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        """
+        Add a trailing slash to path if it doesn't have one
+        """
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            """
+            Add a trailing slash to excluded_path if it doesn't have one
+            """
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization Header method
