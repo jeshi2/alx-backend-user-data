80c44abcbbdc757b2f5282089624452658a276cb
jeshi2 <jamesvean19@gmail.com>
2024-01-22 21:28:51 +0300

    wow

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,7 @@ if getenv("AUTH_TYPE"):
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     """ Before request handler
@@ -27,9 +28,13 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
 
-    if request.path not in excluded_paths and not auth.require_auth(request.path, excluded_paths):
+    if request.path not in excluded_paths and not auth.require_auth(
+            request.path, excluded_paths):
         return
 
     if auth.authorization_header(request) is None:
@@ -37,6 +42,7 @@ def before_request():
 
     if auth.current_user(request) is None:
         abort(403)
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
