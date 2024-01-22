048e8df44d956ddfc208a8b79b918aaa7ce240b1
jeshi2 <jamesvean19@gmail.com>
2024-01-22 21:45:28 +0300

    user credential

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,23 @@ class BasicAuth(Auth):
             return decoded_value
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract User Credentials method
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+
+        """
+        Check if the decoded value contains ":"
+        """
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        """
+        Split the decoded value into email and password
+        """
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
