a7aa806ce8f8724a99d182fe821460fdb687bea5
jeshi2 <jamesvean19@gmail.com>
2024-01-24 12:24:00 +0300

    delte

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -38,14 +38,11 @@ def session_auth_login():
             return jsonify(error="wrong password"), 401
     return jsonify(error="no user found for this email"), 404
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
-    """ DELETE /auth_session/logout
-    Return:
-      - Response
+    """ Log out route
     """
-    from api.v1.app import auth
     if auth.destroy_session(request):
         return jsonify({}), 200
-    abort(404)
+    else:
+        abort(404)
