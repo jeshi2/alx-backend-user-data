a031a8a757f56bdfc5912aedb585c569538abe07
jeshi2 <jamesvean19@gmail.com>
2024-01-24 12:31:36 +0300

    mmh

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-from api.v1.views.session_auth import *
-from api.v1.views.users import *
-from api.v1.views.index import *
+from api.v1.views.session_auth import app_views as session_auth_views
+from api.v1.views.users import app_views as users_views
+from api.v1.views.index import app_views as index_views
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
