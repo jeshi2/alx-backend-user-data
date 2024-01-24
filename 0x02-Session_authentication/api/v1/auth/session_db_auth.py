3c9cb6b8f2bc21ea7557a25ae8dc75f6dc0aeba7
jeshi2 <jamesvean19@gmail.com>
2024-01-24 13:01:58 +0300

    sessiondb

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+SessionDBAuth module
+"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from api.v1.app import auth
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ SessionDBAuth class
+    """
+    def create_session(self, user_id=None):
+        """ Create a new session and store it in the database
+        """
+        session_id = super().create_session(user_id)
+        if session_id:
+            user_session = UserSession(user_id=user_id, session_id=session_id)
+            user_session.save()
+            return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Return the User ID by requesting UserSession in the database
+        """
+        if session_id is None:
+            return None
+
+        user_session = UserSession.search({'session_id': session_id})
+        if not user_session or not user_session[0].user_id:
+            return None
+
+        session_duration = self.session_duration
+
+        if session_duration <= 0:
+            return user_session[0].user_id
+
+        created_at = user_session[0].created_at
+        expiration_time = created_at + timedelta(seconds=session_duration)
+
+        if expiration_time < datetime.utcnow():
+            return None
+
+        return user_session[0].user_id
+
+    def destroy_session(self, request=None):
+        """ Destroy the UserSession based on the Session ID from the request cookie
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        user_session = UserSession.search({'session_id': session_id, 'user_id': user_id})
+
+        if not user_session:
+            return False
+
+        user_session[0].delete()
+        return True
---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+UserSession module
+"""
+from models.base import Base
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+
+
+class UserSession(Base):
+    """ UserSession class
+    """
+    __tablename__ = 'user_sessions'
+
+    user_id = Column(String(60), nullable=False)
+    session_id = Column(String(60), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize UserSession instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id', "")
+        self.session_id = kwargs.get('session_id', "")
