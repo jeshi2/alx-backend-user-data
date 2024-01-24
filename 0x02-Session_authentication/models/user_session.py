f7e723f1771a6178c8da847bbebfea5393fecb42
jeshi2 <jamesvean19@gmail.com>
2024-01-24 13:39:49 +0300

    fail check3

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -2,7 +2,6 @@
 """
 UserSession module
 """
-
 from models.base import Base
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
@@ -10,33 +9,19 @@ from sqlalchemy import Column, String, DateTime
 
 class UserSession(Base):
     """
-    UserSession class represents the storage of user sessions in the database.
-
-    Attributes:
-        user_id (str): The ID of the associated user.
-        session_id (str): The unique identifier for the user session.
-        created_at (DateTime): The timestamp indicating when the user session was created.
-
+     class represents the storage of user sessions in the database
     """
 
     __tablename__ = 'user_sessions'
 
     user_id = Column(String(60), nullable=False)
     session_id = Column(String(60), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime,
+                        default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args: list, **kwargs: dict):
         """
-        Initialize a new UserSession instance.
-
-        Args:
-            *args (list): Variable length argument list.
-            **kwargs (dict): Arbitrary keyword arguments.
-
-        Keyword Args:
-            user_id (str): The ID of the associated user.
-            session_id (str): The unique identifier for the user session.
-
+        Initialize UserSession instance
         """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id', "")
