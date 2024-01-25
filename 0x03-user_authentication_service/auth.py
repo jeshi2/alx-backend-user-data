81bf503b944e2790928c60571b6bd151b0f9c43c
jeshi2 <jamesvean19@gmail.com>
2024-01-25 22:54:58 +0300

    hass password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+auth pass
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
